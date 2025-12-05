85f7a8d7ec1fe2dba9e4df47cc349eadc050ffbb
Narayan-Choudhary1 <narayanofficial.1901ch@gmail.com>
2025-12-05 14:04:16 +0000

    inserting data into Items table

--- answer.sql
+++ answer.sql
@@ -15,3 +15,13 @@ CREATE TABLE items (
     item_category VARCHAR(50),
     item_name VARCHAR(100)
 );
+
+-- 3. Insert Data into Items Table 
+INSERT INTO items (store_id, item_id, item_category, item_name) VALUES
+('a', 'a1', 'pants', 'denim pants'),
+('a', 'a2', 'tops', 'blouse'),
+('f', 'f1', 'table', 'coffee table'),
+('f', 'f5', 'chair', 'lounge chair'),
+('f', 'f6', 'chair', 'armchair'),
+('d', 'd2', 'jewelry', 'bracelet'),
+('b', 'b4', 'earphone', 'airpods');
